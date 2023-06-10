7164c232f9c72bf419a0277f7f6e23d4b3f2ae01
Pramudya Dimas Wilanda <Pramudya.223040005@mail.unpas.ac.id>
2023-06-10 23:45:58 +0700

    Tubes Pw

--- Tubespw/category.php
+++ Tubespw/category.php
@@ -20,7 +20,7 @@ include('includes/config.php');
    <!-- Bootstrap core CSS -->
    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-   <!-- Custom styles for this template -->
+
    <link href="css/modern-business.css" rel="stylesheet">
    <link rel="stylesheet" href="css/icons.css">
 </head>
