c8b4ccf58b1b53cb9f221e054a376638050c32b1
Pramudya Dimas Wilanda <Pramudya.223040005@mail.unpas.ac.id>
2023-06-10 23:19:47 +0700

    Tubes PW

---
+++ Tubespw/includes/footer.php
@@ -0,0 +1,46 @@
+<div class="footer-area pt-3 bg-white mb-5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <ul class="float-left list-unstyled ">
+                    <li class="mb-2"><a href="#" class="text-dark">Mobile Legends</a></li>
+                    <li class="mb-2"><a href="#" class="text-dark">Pubg</a></li>
+                    <li class="mb-2"><a href="#" class="text-dark">Valorant</a></li>
+                    <li class="mb-2"><a href="#" class="text-dark">Dota 2</a></li>
+                    <li class="mb-2"><a href="#" class="text-dark">Wild Rift</a></li>
+
+
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en'
+            }, 'google_translate_element');
+        }
+    </script>
+    <style>
+        .goog-logo-link {
+            display: none !important;
+        }
+
+        .goog-te-gadget {
+            color: transparent;
+        }
+
+        .goog-te-gadget .goog-te-combo {
+            margin: 0px 0;
+            padding: 8px;
+            color: #000;
+            background: #eeee;
+        }
+
+        #google_translate_element {
+            padding-top: 13px;
+            position: absolute;
+            top: 7px;
+            right: 100px;
+        }
+    </style>
